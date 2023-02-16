e675ac96df36b5a3bd2eb5704f3d6d1591f39248
NatBabi <ntile312@gmail.com>
2023-02-16 13:11:54 -0800

    Task-4 and Task-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Prints the text by using printf()
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is piece of art,\n");
+return (0);
+}
