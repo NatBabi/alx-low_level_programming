fe9848f73843e08efda4d616a1307bc9a654ce71
NatBabi <ntile312@gmail.com>
2023-02-27 13:35:52 -0800

    modify the main.h

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,16 +3,17 @@
 /**
  * _puts - prints a string followed by a new line..
  *
- *@str: string which will be displayed.
+ *@str: the string to print.
  *
  */
+
 void _puts(char *str)
 {
+	int i;
 
-	while (*str != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
