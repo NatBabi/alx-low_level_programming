b57da1f2b81987b310a2cdac3aed9f7337422975
NatBabi <ntile312@gmail.com>
2023-06-14 16:02:00 -0700

    Task-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,10 +20,10 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc(sizeof(char) * size);
 
-	if (s == 0)
-		return (0);
+	if (s == NULL)
+		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size - 1); i++)
 		*(s + i) = c;
 
 	*(s + i) = '\0';
